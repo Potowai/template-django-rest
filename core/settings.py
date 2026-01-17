43d571887d5608276ed6c83500bc64ff133db1c2
Potowai <81572547+Potowai@users.noreply.github.com>
2026-01-17 14:01:11 +0100

    Add whitenoise middleware back for production

--- core/settings.py
+++ core/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # CORS Middleware should be placed as high as possible
     'corsheaders.middleware.CorsMiddleware',
